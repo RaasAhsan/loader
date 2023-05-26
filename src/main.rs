b75dd17a04617977c24d7b9c379719c256825d28
Raas Ahsan <2617549+RaasAhsan@users.noreply.github.com>
2023-05-26 08:54:10 +0000

    Load ELF data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,148 @@
+use std::{env, fs::File, os::fd::AsRawFd};
+
+use elf::elf64::{self, header::ProgramHeader};
+use memmap2::MmapOptions;
+
 fn main() {
-    println!("Hello, world!");
+    // let args = env::args().collect::<Vec<_>>();
+    // if args.len() < 2 {
+    //     panic!("Expected path to program");
+    // }
+
+    // let path = &args[1];
+
+    let file = File::open("./examples/a.out").expect("failed to open executable");
+
+    let mmap =
+        unsafe { MmapOptions::new().map(&file) }.expect("failed to map executable into memory");
+
+    let headers = elf64::header::Headers::parse(&mmap).expect("failed to parse elf headers");
+
+    if headers.header.e_type != 0x02 {
+        panic!("only position-dependent executables are supported currently");
+    }
+
+    let mut loadable_hdrs = vec![];
+    for hdr in headers.program_headers.iter() {
+        match hdr.p_type {
+            0x01 => {
+                loadable_hdrs.push(hdr);
+            }
+            0x02 => {
+                panic!("dynamic linking not supported currently");
+            }
+            _ => {}
+        }
+    }
+
+    if loadable_hdrs.is_empty() {
+        panic!("no loadable segments found");
+    }
+
+    let (base, size) = get_initial_memory_map(&loadable_hdrs);
+
+    // Allocate a continguous region of virtual memory so that virtual addresses are intact.
+    initialize_mapping(base, size);
+    load_segments(&file, &loadable_hdrs);
+
+    // println!("{:?}", alloc.as_range());
+
+    let entrypoint = headers.header.e_entry;
+    println!("Jumping to entrypoint {:08x}", entrypoint);
+
+    // let byte = unsafe { *(0x400000 as *const u8) };
+    // println!("{:02x}", byte);
+
+    let start: fn() -> () = unsafe { std::mem::transmute(entrypoint as *const u8) };
+    start();
+}
+
+fn get_initial_memory_map(hdrs: &[&ProgramHeader]) -> (u64, usize) {
+    let lowest = hdrs
+        .iter()
+        .min_by(|x, y| {
+            let xv = x.p_vaddr;
+            let yv = y.p_vaddr;
+            xv.cmp(&yv)
+        })
+        .map(|h| h.p_vaddr)
+        .unwrap();
+    let highest = hdrs
+        .iter()
+        .max_by(|x, y| {
+            let xv = x.p_vaddr;
+            let yv = y.p_vaddr;
+            xv.cmp(&yv)
+        })
+        .map(|h| h.p_vaddr + h.p_memsz)
+        .unwrap();
+    (lowest, (highest - lowest) as usize)
+}
+
+fn initialize_mapping(base: u64, length: usize) {
+    let mapped_addr = unsafe {
+        libc::mmap(
+            base as *mut libc::c_void,
+            length as libc::size_t,
+            libc::PROT_NONE,
+            libc::MAP_FIXED_NOREPLACE | libc::MAP_ANONYMOUS | libc::MAP_PRIVATE,
+            -1,
+            0,
+        )
+    };
+
+    if mapped_addr == libc::MAP_FAILED {
+        panic!("failed to map segment {}", unsafe {
+            *libc::__errno_location()
+        });
+    }
+}
+
+fn load_segments(file: &File, hdrs: &[&ProgramHeader]) {
+    for hdr in hdrs.iter() {
+        let addr = hdr.p_vaddr;
+        let aligned_addr = addr & !0xfff;
+        let length = hdr.p_filesz + (addr - aligned_addr);
+        let h_flags = hdr.p_flags;
+        let h_offset = (hdr.p_offset & !0xfff) as i64;
+
+        println!("Attempting to map {:08x}-{:08x}", addr, addr + hdr.p_memsz);
+
+        let mut prot: libc::c_int = 0;
+        if h_flags & 0b001 != 0 {
+            prot |= libc::PROT_EXEC;
+        }
+        if h_flags & 0b010 != 0 {
+            prot |= libc::PROT_WRITE;
+        }
+        if h_flags & 0b100 != 0 {
+            prot |= libc::PROT_READ;
+        }
+
+        let mapped_addr = unsafe {
+            libc::mmap(
+                aligned_addr as *mut libc::c_void,
+                length as libc::size_t,
+                prot,
+                libc::MAP_FIXED | libc::MAP_PRIVATE,
+                file.as_raw_fd(),
+                h_offset,
+            )
+        };
+
+        if mapped_addr == libc::MAP_FAILED {
+            panic!("failed to map segment {}", unsafe {
+                *libc::__errno_location()
+            });
+        }
+
+        println!(
+            "Mapped {:08x}:{:08x} to {:08x}-{:08x} with prot {:?}",
+            h_offset,
+            length,
+            mapped_addr as u64,
+            (mapped_addr as u64) + length,
+            h_flags
+        );
+    }
 }
